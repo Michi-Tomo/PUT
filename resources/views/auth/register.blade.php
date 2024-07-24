c1fe1556a5c238df5a5b28cc93c185c37b19d525
ako-shima <a8344212@gmail.com>
2024-07-24 23:00:12 +0800

    マイページ画面のデザインを変更しました

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,165 +2,139 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="bike-illustration"></div>
-            <div class="road"></div>
-            <div class="card">
-                <div class="card-header">{{ __('新規登録') }}</div>
+<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="col-md-6 text-center">
+        <img src="{{ asset('images/put.png') }}" alt="Register Image" class="img-fluid mb-4">
+        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('ニックネーム') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('メールアドレス') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('電話番号') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
-
-                                @error('phone')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('パスワード') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('確認パスワード') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="乗客" class="col-md-4 col-form-label text-md-end">{{ __('乗客') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="乗客" type="radio" name="user_type" value="0" onclick="handleClick(this);">
-                            </div>
-
-                            <label for="ドライバー" class="col-md-4 col-form-label text-md-end">{{ __('ドライバー') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="ドライバー" type="radio" name="user_type" value="1" onclick="handleClick(this);">
-                            </div>
-                        </div>
-
-                        {{-- ドライバー専用の追加項目 --}}
-                        <div id="driver_infos" style="display: none">
-                            <div class="row mb-3">
-                                <input id="driver_image" type="file" name="driver_image" class="col-md-4 col-form-label text-md-end">{{ __('登録用の顔写真') }}
-
-                            <div class="col-md-6">
-                                @error('driver_image')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="age" class="col-md-4 col-form-label text-md-end">{{ __('年齢') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="age" type="text" class="form-control @error('age') is-invalid @enderror" name="age" value="{{ old('age') }}" autocomplete="age" autofocus>
-
-                                @error('age')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="driver_license" class="col-md-4 col-form-label text-md-end">{{ __('運転免許証') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="driver_license" type="text" class="form-control @error('driver_license') is-invalid @enderror" name="driver_license" value="{{ old('driver_license') }}" autocomplete="driver_license" autofocus>
-
-                                @error('driver_license')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="license_plate" class="col-md-4 col-form-label text-md-end">{{ __('車両番号') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="license_plate" type="text" class="form-control @error('license_plate') is-invalid @enderror" name="license_plate" value="{{ old('license_plate') }}" autocomplete="license_plate" autofocus>
-
-                                @error('license_plate')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        </div>
-
-                        <div class="row mb-0 justify-content-center">
-                            <div class="col-md-6 text-center">
-                                <button type="submit" class="btn btn-pink btn-lg btn-block">
-                                    <i class="bi bi-pencil-square"></i> {{ __('新規登録する') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="row mb-3">
+                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('ニックネーム') }}</label>
+                <div class="col-md-8">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
             </div>
-        </div>
+
+            <div class="row mb-3">
+                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('メールアドレス') }}</label>
+                <div class="col-md-8">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('電話番号') }}</label>
+                <div class="col-md-8">
+                    <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+                    @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('パスワード') }}</label>
+                <div class="col-md-8">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('確認パスワード') }}</label>
+                <div class="col-md-8">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-md-4 col-form-label text-md-end">{{ __('') }}</label>
+                <div class="col-md-8 d-flex justify-content-center align-items-center">
+                    <div class="form-check form-check-inline">
+                        <input id="乗客" type="radio" name="user_type" value="0" class="form-check-input" onclick="handleClick(this);">
+                        <label for="乗客" class="form-check-label">{{ __('乗客') }}</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input id="ドライバー" type="radio" name="user_type" value="1" class="form-check-input" onclick="handleClick(this);">
+                        <label for="ドライバー" class="form-check-label">{{ __('ドライバー') }}</label>
+                    </div>
+                </div>
+            </div>
+
+            <div id="driver_infos" style="display: none">
+                <div class="row mb-3">
+                    <label for="driver_image" class="col-md-4 col-form-label text-md-end">{{ __('登録用の顔写真') }}</label>
+                    <div class="col-md-8">
+                        <input id="driver_image" type="file" name="driver_image" class="form-control @error('driver_image') is-invalid @enderror">
+                        @error('driver_image')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="age" class="col-md-4 col-form-label text-md-end">{{ __('年齢') }}</label>
+                    <div class="col-md-8">
+                        <input id="age" type="text" class="form-control @error('age') is-invalid @enderror" name="age" value="{{ old('age') }}" autocomplete="age" autofocus>
+                        @error('age')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="driver_license" class="col-md-4 col-form-label text-md-end">{{ __('運転免許証') }}</label>
+                    <div class="col-md-8">
+                        <input id="driver_license" type="text" class="form-control @error('driver_license') is-invalid @enderror" name="driver_license" value="{{ old('driver_license') }}" autocomplete="driver_license" autofocus>
+                        @error('driver_license')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="license_plate" class="col-md-4 col-form-label text-md-end">{{ __('車両番号') }}</label>
+                    <div class="col-md-8">
+                        <input id="license_plate" type="text" class="form-control @error('license_plate') is-invalid @enderror" name="license_plate" value="{{ old('license_plate') }}" autocomplete="license_plate" autofocus>
+                        @error('license_plate')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-0 justify-content-center">
+                <div class="col-md-6 text-center">
+                    <button type="submit" class="btn btn-pink btn-lg btn-block">
+                        <i class="bi bi-pencil-square"></i> {{ __('新規登録する') }}
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
 
@@ -188,6 +162,9 @@
         background-color: #141414;
         border-color: #000000;
         color: #ffffff;
+    }
+    .container {
+        min-height: 100vh;
     }
 </style>
 
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,37 +7,67 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
     <style>
         body {
-            font-family: Arial, sans-serif;
+            font-family: 'Helvetica Neue', Arial, sans-serif;
             margin: 0;
             padding: 0;
             display: flex;
             flex-direction: column;
             align-items: center;
-            justify-content: space-between;
+            justify-content: flex-start;
             height: 100vh;
-            background-color: #f0f0f0;
+            background-color: #e9ecef;
         }
 
         .container {
             text-align: center;
-            width: 100%;
+            margin-right: 31px;
             flex: 1;
             display: flex;
             flex-direction: column;
+            align-items: center;
             justify-content: center;
+            padding: 20px;
+            margin-bottom: 60px; /* メニューバーに被らないように調整 */
         }
 
         .profile-edit {
-            background: #fff;
             padding: 20px;
-            margin: 20px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            width: 80%;
+            max-width: 500px;
+        }
+
+        form input {
+            width: 100%;
+            padding: 15px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 1rem;
+        }
+
+        .icon-buttons {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+
+        .icon-button {
+            background: none;
+            border: none;
+            color: #6c757d;
+            cursor: pointer;
+            font-size: 2.5rem; /* アイコンを大きくする */
+            margin: 0 15px;
+            transition: color 0.3s;
+        }
+
+        .icon-button:hover {
+            color: #343a40;
         }
 
         .menu-bar {
             width: 100%;
-            background-color: #333;
+            background-color: #343a40;
             color: white;
             display: flex;
             justify-content: space-around;
@@ -61,44 +91,35 @@
         }
 
         .menu-item a i {
-            font-size: 1.5rem;
+            font-size: 2rem;
             margin-bottom: 5px;
         }
 
-        form input {
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        form button {
-            padding: 10px 20px;
-            border: none;
-            background-color: #333;
-            color: white;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        form button:hover {
-            background-color: #555;
+        .menu-item a span {
+            font-size: 1.2rem;
         }
     </style>
 </head>
 <body>
+    <img src="{{ asset('images/put.png') }}" alt="Header Image" class="header-image">
     <div class="container">
         <div class="profile-edit">
-            <h1>プロフィール編集</h1>
-            <form action="{{ route('profile.update') }}" method="POST">
+            <form id="profile-form" action="{{ route('profile.update') }}" method="POST">
                 @csrf
                 <input type="text" name="name" value="{{ old('name', $user->name) }}" placeholder="名前" required>
                 <input type="email" name="email" value="{{ old('email', $user->email) }}" placeholder="メールアドレス" required>
                 <input type="text" name="phone" value="{{ old('phone', $user->phone) }}" placeholder="電話番号">
                 <input type="password" name="password" placeholder="パスワード（変更する場合のみ）">
                 <input type="password" name="password_confirmation" placeholder="パスワード確認">
-                <button type="submit">更新</button>
+                
+                <div class="icon-buttons">
+                    <button type="submit" class="icon-button" title="更新">
+                        <i class="bi bi-check-circle"></i>
+                    </button>
+                    <a href="/mypage" class="icon-button" title="キャンセル">
+                        <i class="bi bi-x-circle"></i>
+                    </a>
+                </div>
             </form>
         </div>
     </div>
@@ -129,5 +150,25 @@
             </a>
         </div>
     </div>
+
+    <script>
+        document.getElementById('profile-form').addEventListener('submit', function(event) {
+            event.preventDefault(); // フォームのデフォルト送信を防ぐ
+            // フォームを送信
+            fetch(this.action, {
+                method: this.method,
+                body: new FormData(this)
+            }).then(response => {
+                if (response.ok) {
+                    window.location.href = '/mypage'; // 成功したらプロフィール画面にリダイレクト
+                } else {
+                    alert('更新に失敗しました。再度お試しください。');
+                }
+            }).catch(error => {
+                console.error('Error:', error);
+                alert('更新に失敗しました。再度お試しください。');
+            });
+        });
+    </script>
 </body>
 </html>
