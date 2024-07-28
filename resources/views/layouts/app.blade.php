cf40e911205bbaae1f4e4a212b6bed8fc07356b8
Michi-Tomo <michihiro.ishihara.0926@gmail.com>
2024-07-28 15:23:21 +0800

    ファビコンを修正しました

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
 
     @stack('css')
 
